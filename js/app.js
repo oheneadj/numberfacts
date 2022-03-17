ca445d2653de592959268fce37b54cdb00ac857e
oheneadj <oheneadjei.dev@gmail.com>
2022-03-17 12:47:39 +0000

    commit for number facts

--- js/app.js
+++ js/app.js
@@ -26,4 +26,45 @@ function getNumberFact(){
 
 
 
+//Check to see if date is selected
+dateInput.addEventListener('change', getDateFact);
+
+
+function getDateFact(){
+
+    let number = dateInput.value;
+
+    // String(number);
+
+    const date = number.split('-');
+
+
+    const url = `http://numbersapi.com/${date[1]}/${date[2]}/date`;
+    fetch (url)
+    .then(response => response.text())
+    .then(data => {
+        if(number !== '' ){
+            factText.innerText = data;
+        }
+    })
+    .catch(err => console.log(err));
+}
+
+
+//Check to see if button is clicked
+random.addEventListener('click', getRandomFact);
+
+
+
+function getRandomFact(){
+    console.log("Button was clicked")
+    const url = `http://numbersapi.com/random/trivia`;
+    fetch (url)
+    .then(response => response.text())
+    .then(data => {
+        factText.innerText = data;
+    })
+    .catch(err => console.log(err));
+}
+
 
